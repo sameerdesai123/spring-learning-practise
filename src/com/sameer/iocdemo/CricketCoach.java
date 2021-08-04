e92a14d4ca7b86cce12155a392f841fd59220025
Sameer Desai <sameerdesai1008@gmail.com>
2021-08-04 12:35:04 +0530

    Injecting literal values via setters

--- src/com/sameer/iocdemo/CricketCoach.java
+++ src/com/sameer/iocdemo/CricketCoach.java
@@ -3,7 +3,8 @@ package com.sameer.iocdemo;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
-	
+	private String emailAddress;
+	private String team;
 	
 	public CricketCoach() {
 		super();
@@ -19,6 +20,24 @@ public class CricketCoach implements Coach {
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("----- Inside cricket coach setter method");
 		this.fortuneService = fortuneService;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("----- Setting email address");
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("----- Setting team");
+		this.team = team;
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
 	}
 
 	@Override
--- src/com/sameer/iocdemo/SetterDemoApp.java
+++ src/com/sameer/iocdemo/SetterDemoApp.java
@@ -5,17 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SetterDemoApp {
 
 	public static void main(String[] args) {
-		// Load xml config file
+		// Load xml config file5
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// Retrive bean from container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
 		// call methods
 		
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
-		
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 		// close cxt
 		context.close();
 
